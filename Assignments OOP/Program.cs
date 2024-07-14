72f2be694a56703c94e706f44a270eb33f5fa466
entsarahmed <entesara500@gmail.com>
2024-07-14 21:30:10 +0300

    2. Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.

--- Assignments OOP/Program.cs
+++ Assignments OOP/Program.cs
@@ -13,6 +13,28 @@
     }
 
     #endregion
+
+    #region 2. Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+    struct Person
+    {
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string _Name, int _Age)
+        {
+            Name = _Name;
+            Age = _Age;
+        }
+
+
+        //Display Person Of Data
+        public override string ToString()
+        {
+            return $"Person Name: {Name}, Person Age: {Age}";
+        }
+    }
+
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
@@ -20,7 +42,23 @@
             #region 1.Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
 
             //GetValues();-->Return Array of value for Enmum
-            foreach (var item in Enum.GetValues(typeof(WeekDays)))
+            //foreach (var item in Enum.GetValues(typeof(WeekDays)))
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
+
+            #region 2. Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+            Person[] person =
+            {//Object intializer
+                new Person("Ahmed",23),
+                new Person("Ali",22),
+                new Person("Mohmed",30)
+
+            };
+            foreach (var item in  person)
             {
                 Console.WriteLine(item);
             }
