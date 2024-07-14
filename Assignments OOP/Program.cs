21c60a1955df440b29a6abd5feaee732639fe2b3
entsarahmed <entesara500@gmail.com>
2024-07-14 20:26:39 +0300

    Add project files.

---
+++ Assignments OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignments_OOP
+{
+    #region 1.Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
+
+
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1.Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members. Then, write a C# program that prints out all the days of the week using this enum.
+
+
+            #endregion
+
+        }
+    }
+}
